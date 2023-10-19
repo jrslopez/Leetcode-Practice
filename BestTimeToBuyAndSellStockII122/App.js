631142fbda1c1fcef33bcbb7ef9d722c8adab4ed
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2023-10-18 22:06:17 -0400

    added solution for 122

--- BestTimeToBuyAndSellStockII122/App.js
+++ BestTimeToBuyAndSellStockII122/App.js
@@ -1,1 +1,13 @@
-console.log("initial")
+let prices = [1, 2, 3, 4, 5]
+
+let profit = 0
+
+for (let i = 1; i < prices.length; i++) {
+  let previousPrice = prices[i - 1]
+  let currentPrice = prices[i]
+  if (previousPrice < currentPrice) {
+    profit += currentPrice - previousPrice
+  }
+}
+
+console.log(profit)
